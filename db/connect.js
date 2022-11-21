6b555dca852edf03b7e6d4255fb25e0ecf500e54
sajda <sajda.kbir@gmail.com>
2022-11-21 22:43:51 +0530

    connected with mongodb

---
+++ db/connect.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import colors from 'colors';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+
+
+export default connectDB;
