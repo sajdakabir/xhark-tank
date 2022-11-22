49a909e82170acb25b2959bce43c6f9a4926f460
sajda <sajda.kbir@gmail.com>
2022-11-22 12:03:18 +0530

    create pitch apt done

--- controllers/pitches.js
+++ controllers/pitches.js
@@ -3,5 +3,16 @@ import Pitch from '../models/pitchModel.js';
 
 
 export const createPost=async(req,res)=>{
-    console.log("running");
+    const pitch=req.body;
+    const newPitch=new Pitch({...pitch,createdAt: new Date().toISOString()});
+
+    try {
+        await newPitch.save();
+        res.status(201).json({
+            message:'Pitch created successfully',
+            _id:newPitch._id,
+        });
+    } catch (error) {
+        res.status(400).json({message:error.message});
+    }
 };
