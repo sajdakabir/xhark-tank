97c0c5ffa1b6623de81bd84592e050de66e9d463
sajda <sajda.kbir@gmail.com>
2023-02-11 13:12:07 +0530

    pitch idea button done

---
+++ client/src/components/PitchFrom/PitchFrom.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { Button, Modal, Form } from "react-bootstrap";
+import './PitchFrom.css';
+import AddIcon from '@mui/icons-material/Add';
+function PitchFrom() {
+    const [show,setShow]=useState(false);
+    const handleShow=()=>{
+        setShow(true);
+    }
+    const handleClose=()=>{
+        setShow(false);
+    }
+  return (
+    <>
+       <Button className="button f" onClick={handleShow}>
+        <AddIcon className='addIcon'  sx={{ mr: 4 }}></AddIcon>
+         Pitch Your Idea
+      </Button>
+    </>
+  )
+}
+
+export default PitchFrom
