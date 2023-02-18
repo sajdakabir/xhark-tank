a5466b1c4dd7178f15c2cf849a0192ae52c8e1b2
sajda <sajda.kbir@gmail.com>
2023-02-19 00:08:22 +0530

    added the defualt boxes

---
+++ client/src/components/Pitchs/DefaultBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Pitchs.css";
+function DefaultBox() {
+  return (
+    <>
+    <div className="default"></div>;
+    <div className="default"></div>;
+    <div className="default"></div>;
+    </>
+  )
+ 
+    
+  
+}
+
+export default DefaultBox;
